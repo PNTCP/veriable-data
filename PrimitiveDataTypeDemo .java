1c4378cff2d4ce441c3457f580d9fe61ec314678
pntcp <nattaponchanprom@gmail.com>
2026-02-03 16:52:50 +0700

    first

---
+++ PrimitiveDataTypeDemo .java
@@ -0,0 +1,31 @@
+public class PrimitiveDataTypeDemo {
+
+    public static void main(String[] args) {
+
+        // 1. Integer types (จำนวนเต็ม)
+        byte b = 100;
+        short s = 32000;
+        int i = 100000;
+        long l = 1000000000L;
+
+        // 2. Floating-point types (ทศนิยม)
+        float f = 10.5f;
+        double d = 99.99;
+
+        // 3. Character type (ตัวอักษร)
+        char c = 'A';
+
+        // 4. Boolean type (ค่าตรรกะ)
+        boolean isJavaFun = true;
+
+        // แสดงผล
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + isJavaFun);
+    }
+}
